9bc1881e8a6142054700d1749521a7c01d86929a
Mohammed Alsayed Omar <dev.mohammedsayedomar@gmail.com>
2017-03-29 06:49:28 +0200

    chore: Added more assets folder

---
+++ Assets/VRND-Course2-Project/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    //The code is written by me, Mohammed Omar but the audio sources were downloaded from "http://soundbible.com/tags-city.html"
+
+    public Vector2 minMaxRandomTime;
+
+    AudioSource[] audioSources;
+    int audioSourceLength;
+
+
+    void Start () {
+        audioSources = GetComponents<AudioSource>();
+        StartCoroutine(ChooseRandomTrack(Random.Range(minMaxRandomTime.x, minMaxRandomTime.y)));
+
+        audioSourceLength = audioSources.Length;
+    }
+
+    IEnumerator ChooseRandomTrack(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        int randomIndex = Random.Range(0, audioSourceLength);
+        if (!audioSources[randomIndex].isPlaying)
+            audioSources[randomIndex].Play();
+
+        StartCoroutine(ChooseRandomTrack(Random.Range(minMaxRandomTime.x, minMaxRandomTime.y)));
+    }
+}
